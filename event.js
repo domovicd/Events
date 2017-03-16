0f6c555c0e50d4e737bfc1466f35f9232cbdb228
domovicd <domovic84@gmail.com>
2017-03-16 10:19:21 +0200

    Add files via upload

---
+++ event.js
@@ -0,0 +1,25 @@
+/*var mojGumbVar=document.getElementById("mojGumb");
+mojGumbVar.onclick=function () {
+    alert = ("Ovaj tekst če biti ispisan");
+}
+*/
+
+document.getElementById("mojgumb").addEventListener("click",myCall);
+var moj_string="pozvali ste me";
+function myCall() {
+    document.getElementById("mojgumb2").innerHTML=moj_string;
+}
+
+function f1() {
+    alert("Aj radi kasnije se budemo igrali");
+}
+function f2() {
+    alert("Aj radi kasnije se budemo igrali");
+}
+function f3() {
+    alert("Aj radi kasnije se budemo igrali");
+}
+
+function funkci() {
+    alert("Sve je ucitano!!");
+})
