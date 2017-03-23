227c6cb2449a209305e9ba10755a0b19297e4cc2
domovicd <domovic84@gmail.com>
2017-03-23 10:22:11 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,44 @@
+/**document.getElementById("s").addEventListener("click",funk());
+function funk() {
+    var fruits=["Jabuka","Banana","Breskva","Smokva"];
+        for (var i = 0; i <fruits.length; i++) {
+         document.getElementById("ptag").innerHTML+=fruits[i]+"<br>";
+     }
+}
+**/
+/**var languages=["HTML","CSS","JavaScript","Python","Ruby"];
+for(var i=0; i<languages.length;i++){
+    console.log(languages[i]);
+    if(languages[i]=="JavaScript"){
+        document.getElementById("ptag").innerHTML="JavaScript"+"<br>"+"Duzina je " +languages.length;
+    }
+}
+ **/
+/**var dod=["grr","drr","crr","frr","krr","zrr"];
+dod.push("jrr");
+document.getElementById("ptag").innerHTML= dod+"<br>";
+**/
+
+/**var bob={
+    name:"Mizi",
+    age:30
+};
+var susan={
+    name:"Suzie",
+    age:25
+};
+console.log(bob.name,susan.age);
+ **/
+
+var bob={
+    age:25
+    }
+};
+var susan={
+    age:3
+};
+function () {
+    
+}
+
+
